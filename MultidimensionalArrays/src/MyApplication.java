ea7d346acdf260afcdd7b823b5a8a9be3584f30f
Eric <Epineda773@protonmail.com>
2020-05-09 23:56:35 -0500

    Multidimensional array section

---
+++ MultidimensionalArrays/src/MyApplication.java
@@ -0,0 +1,14 @@
+
+public class MyApplication {
+	public static void main(String[] args) {
+		
+		//Example of a One dimensional array
+		int[] oneDimensionalArray = {12, 49, 922};
+		System.out.println(oneDimensionalArray[1]);
+		
+		//Example of a multidimensional array
+		int[][] multiDimensionalArray = {{12, 49, 922}, {23, 56, 78, 12}, {23, 56, 78, 12}};
+		System.out.println(multiDimensionalArray[1][2]);
+				
+	}
+}
