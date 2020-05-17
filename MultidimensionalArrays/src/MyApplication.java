969410af0e4f8425553893a5186c04a19622d810
Eric <Epineda773@protonmail.com>
2020-05-16 23:46:31 -0500

    section on classes complete + notes

--- MultidimensionalArrays/src/MyApplication.java
+++ MultidimensionalArrays/src/MyApplication.java
@@ -7,8 +7,22 @@ public class MyApplication {
 		System.out.println(oneDimensionalArray[1]);
 		
 		//Example of a multidimensional array
-		int[][] multiDimensionalArray = {{12, 49, 922}, {23, 56, 78, 12}, {23, 56, 78, 12}};
-		System.out.println(multiDimensionalArray[1][2]);
+		int[][] multiDimensionalArray = {{12, 49, 922}, {23, 56, 78, 12}, {12, 34, 56,}};
+		System.out.println(multiDimensionalArray[1][3]);
+		System.out.println(multiDimensionalArray[2][1]);
+		
+		double [][] Doublearray = new double[4][2];
+		
+		Doublearray[3][1] = 2.6;
+		System.out.println(Doublearray[3][1]);
+		
+		//How to iterate through a multidimensional array
+		for(int array=0; array<multiDimensionalArray.length; array++) {
+			for(int item=0; item<multiDimensionalArray[array].length; item++) {
+				System.out.print(multiDimensionalArray[array][item] + "\t");
+			}
+			System.out.println();
+		}
 				
 	}
 }
