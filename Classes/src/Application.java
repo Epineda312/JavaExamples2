33e37aea07a4904bdc0f7a7bd7f6f7c4ac9cf578
Eric <Epineda773@protonmail.com>
2020-05-23 21:58:58 -0500

    add Methods section + notes

--- Classes/src/Application.java
+++ Classes/src/Application.java
@@ -3,12 +3,31 @@ class Methods{
 	
 	String animal;
 	int value;
+	int month;
 	
 	void run() {
+		
 		System.out.println("Running");
 		for(int i=0; i<4; i++) {
 		System.out.println("My " + animal + " is " + value + " years old.");
 		}
+	}
+	
+	int calculateMonthsToBirthday() {
+		int monthsLeft = 12 - month;
+		return monthsLeft;
+	}
+	
+	void talk(String text) {
+		System.out.println(text);
+	}
+	
+	void walk(int speed) {
+		System.out.println("Walking " + speed + " km per hour");
+	}
+	
+	void timer(int minutes, double seconds) {
+		System.out.println("It took " + minutes + "m and " + seconds + "s to get down the hill.");
 	}
 }
 
@@ -20,7 +39,16 @@ public class Application {
 		Methods name = new Methods();
 		name.animal = "Cat";
 		name.value = 9;
-		name.run();
+		//name.run();
+		name.month = 8;
+		int months = name.calculateMonthsToBirthday();
+		name.talk("Hi, I am Steven!");
+		name.walk(10);
+		String hello = "Hello there";
+		name.timer(10,3.64);
+		name.talk(hello);
+		
+		System.out.println("Months until birthday " + months);
 		
 	}
 }
